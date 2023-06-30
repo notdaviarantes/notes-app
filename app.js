8459ada8ecb66e06f7bc53bd793c937d8b13deac
notdaviarantes <daviarantes13@gmail.com>
2023-06-30 11:23:30 -0300

    adding the remove, read and list functions

--- app.js
+++ app.js
@@ -26,24 +26,38 @@ yargs.command({
 yargs.command({
     command: 'remove',
     describe: 'Remove a note',
-    handler: function () {
-        console.log('Removing the note')
+    builder: {
+        title: {
+            describe: 'note title', 
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.removeNotes(argv.title);
     }
 })
 
 yargs.command({
     command: 'list',
     describe: 'List your notes',
-    handler: function () {
-        console.log('Listing out all notes')
+    handler: function (argv) {
+        notes.listNotes(argv.title)
     }
 })
 
 yargs.command({
     command: 'read',
     describe: 'Read a note',
-    handler: function () {
-        console.log('Reading a note')
+    builder: {
+        title: {
+            describe: 'note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.readNote(argv.title)
     }
 })
 
--- notes.js
+++ notes.js
@@ -38,5 +38,36 @@ module.exports = {
     saveNotes: function (notes) {
         const dataJSON = JSON.stringify(notes);
         fs.writeFileSync('notes.json', dataJSON);
+    },
+
+    removeNotes: function(title) {
+        let notes = this.loadNotes();
+
+        const removeNote = notes.filter((note) => { 
+            return note.title !== title
+        })
+
+        if (removeNote < notes) {
+            notes = removeNote;
+            console.log('note removed');
+            this.saveNotes(notes);
+        } else {
+            console.log('note does not exist')
+        }
+    },
+
+    listNotes: function(title) {
+        const notes = this.loadNotes();
+
+        for (let i = 0; i < notes.length; i++) {
+            console.log(notes[i])
+        }
+    },
+
+    readNote: function(title) {
+        const notes = this.loadNotes();
+
+        const noteFound = notes.find((note) => note.title === title)
+        console.log(noteFound);
     }
 }
